3f3831d92e918df0b3e9403bf29dbe7e90572fab
lesdead <lesdead@gmail.com>
2022-12-06 20:32:46 +0200

    all files added

---
+++ src/main/java/domain/FormDate.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Post {
+
+    FormDate formDate = new FormDate();
+
+    public FormDate birthday;
+
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
